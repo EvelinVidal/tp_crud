8004e15b844c26f6fe61826700cf47e6d9ad8a89
EvelinVidal <evelinvidal@tdf.edu.ar>
2023-10-08 23:33:31 -0300

    atributos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CiudadModule } from './ciudad/ciudad.module';
+import { ProfesorModule } from './profesor/profesor.module';
+import { EstudianteModule } from './estudiante/estudiante.module';
+import { AsistenciaModule } from './asistencia/asistencia.module';
+import { ClaseModule } from './clase/clase.module';
+import { EscuelaModule } from './escuela/escuela.module';
 
 @Module({
-  imports: [],
+  imports: [CiudadModule, ProfesorModule, EstudianteModule, AsistenciaModule, ClaseModule, EscuelaModule],
   controllers: [AppController],
   providers: [AppService],
 })
