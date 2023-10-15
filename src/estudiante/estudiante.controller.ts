2d089a5997ec03f8e71206e204b10ba29a83f940
EvelinVidal <evelinvidal@tdf.edu.ar>
2023-10-15 03:53:13 -0300

    estudiante CRUD

--- src/estudiante/estudiante.controller.ts
+++ src/estudiante/estudiante.controller.ts
@@ -1,36 +1,56 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { EstudianteService } from './estudiante.service';
 import { EstudianteDto } from './dto/create-estudiante.dto';
+import { Estudiante } from './entities/estudiante.entity';
 
 @Controller('estudiante')
 export class EstudianteController {
   constructor(private readonly estudianteService: EstudianteService) {}
 
+  //** Crear estudiante */
   @Post('nuevo')
   create(@Body() estudianteDto: EstudianteDto) {
     return this.estudianteService.create(estudianteDto);
   }
 
-  // @Post('con_relacion/:id') 
-  // async createConRelacion(@Body() estudianteDto: EstudianteDto):Promise<boolean> { 
+  // @Post('con_relacion/:id')
+  // async createConRelacion(@Body() estudianteDto: EstudianteDto):Promise<boolean> {
   //   return await this.estudianteService.addClase(estudianteDto);
   // }
-  
-  @Post('agregar-clase') 
-  async agregarClase(@Body() body:any):Promise<any> { 
+
+  //** agregar clase a Estudiante */
+  @Post('agregar-clase')
+  async agregarClase(@Body() body: any): Promise<any> {
     return await this.estudianteService.addClase(body);
   }
-  @Get()
-  async findAll() {
+
+  //** obtener todos los estudiantes */
+  @Get('get-all')
+  async findAll(): Promise<Estudiante[]> {
     return await this.estudianteService.findAll();
   }
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.estudianteService.findOne(+id);
+
+  //** Actualizar estudiante */
+  @Put('modificar-estudiante/:id')
+  async update(
+    @Body() EstudianteDto: EstudianteDto,
+    @Param('id') id: number,
+  ): Promise<string> {
+    return this.estudianteService.update(EstudianteDto, id);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.estudianteService.remove(+id);
+  //** Eliminar estudiante */
+  @Delete('eliminar-estudiante/:id')
+  remove(@Param('id') id: number): Promise<any> {
+    return this.estudianteService.remove(id);
   }
 }
--- src/estudiante/estudiante.service.ts
+++ src/estudiante/estudiante.service.ts
@@ -1,67 +1,112 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { EstudianteDto } from './dto/create-estudiante.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Estudiante } from './entities/estudiante.entity';
-import { Repository } from 'typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
 import { Clase } from 'src/clase/entities/clase.entity';
 import { EstudianteClase } from './entities/clase_estudiante.entity';
 
 @Injectable()
 export class EstudianteService {
-  constructor(@InjectRepository(Estudiante)
-    private estudianteRepository:Repository<Estudiante>,
+  constructor(
+    @InjectRepository(Estudiante)
+    private estudianteRepository: Repository<Estudiante>,
     @InjectRepository(Clase)
-    private claseRepository:Repository<Clase>,
+    private claseRepository: Repository<Clase>,
     @InjectRepository(EstudianteClase)
-    private estudianteClaseRepository:Repository<EstudianteClase>)
-    {}
+    private estudianteClaseRepository: Repository<EstudianteClase>,
+  ) {}
 
- async create(estudianteDto: EstudianteDto) {
-  // const fechaActual = new Date() -> puedo obtener la fecha actual y usarla como parametro en la creación de estudiante. new Estudiante (..., fechaActual)
+  //** CREATE */
+  async create(estudianteDto: EstudianteDto) {
+    // const fechaActual = new Date() -> puedo obtener la fecha actual y usarla como parametro en la creación de estudiante. new Estudiante (..., fechaActual)
     // const estudiante=new Estudiante(estudianteDto.nombre, estudianteDto.apellido, estudianteDto.fecha_nacimiento)
     // const estudianteGuardado : Estudiante = await this.estudianteRepository.save(estudiante)
-    const estudiante : Estudiante = await this.estudianteRepository.save(new Estudiante(estudianteDto.nombre, estudianteDto.apellido, estudianteDto.fecha_nacimiento)) // combina las lineas ant
-    if(estudiante)
-      return `se creó el estudiante: ${estudiante.nombre}`
-    else 
-    return 'no se pudo crear el estudiante '
+    const estudiante: Estudiante = await this.estudianteRepository.save(
+      new Estudiante(
+        estudianteDto.nombre,
+        estudianteDto.apellido,
+        estudianteDto.fecha_nacimiento,
+      ),
+    ); // combina las lineas ant
+    if (estudiante) return `se creó el estudiante: ${estudiante.nombre}`;
+    else return 'no se pudo crear el estudiante ';
   }
 
-//   async createConRelacion(estudianteDto:EstudianteDto):Promise<boolean>{
-//    const clase:Clase = await this.claseRepository.findOne({where: {id:1}})  //consultar si existe la clase x, para eso inyectamos la clase --> linea 13
-//    const estudiante = new Estudiante(estudianteDto.nombre, estudianteDto.apellido, estudianteDto.fecha_nacimiento) // -> creo el estudiante
-//     if (clase)
-//      estudiante.clases = [clase];  //le asignamos la clase al estudiante.
-//     await this.estudianteRepository.save(estudiante) // guardo el estudiante.
-//      if(estudiante)
-// return true
-// else 
-//     return false;
-//   }
+  //   async createConRelacion(estudianteDto:EstudianteDto):Promise<boolean>{
+  //    const clase:Clase = await this.claseRepository.findOne({where: {id:1}})  //consultar si existe la clase x, para eso inyectamos la clase --> linea 13
+  //    const estudiante = new Estudiante(estudianteDto.nombre, estudianteDto.apellido, estudianteDto.fecha_nacimiento) // -> creo el estudiante
+  //     if (clase)
+  //      estudiante.clases = [clase];  //le asignamos la clase al estudiante.
+  //     await this.estudianteRepository.save(estudiante) // guardo el estudiante.
+  //      if(estudiante)
+  // return true
+  // else
+  //     return false;
+  //   }
 
-async addClase(body):Promise<any>{
-  const {claseId,estudianteId} = body;
-  const estudiante = await this.estudianteRepository.findOne({where:{id:estudianteId}}) // busca el estudiante por id 
-  if (!estudiante)
-    return  `no se encontró ese estudiante con el id ${estudianteId}` // si no existe se muestra este texto
-  const clase = await this.claseRepository.findOne({where:{id:claseId}})
-  if(!clase)
-    return "error - no se encontró clase"
-  const clase_estudiante = await this.estudianteClaseRepository.findOne({where:{claseId:claseId,estudianteId:estudianteId}}) // si 
-  if (clase_estudiante)
-    return "el estudiante ya tiene asignada esa clase";
-  return  await this.estudianteClaseRepository.save(new EstudianteClase(estudianteId,claseId));
- }
-
-  findAll() {
-    return `This action returns all estudiante`;
+  //** ADD CLASE */
+  async addClase(body): Promise<any> {
+    const { claseId, estudianteId } = body;
+    const estudiante = await this.estudianteRepository.findOne({
+      where: { id: estudianteId },
+    }); // busca el estudiante por id
+    if (!estudiante)
+      return `no se encontró ese estudiante con el id ${estudianteId}`; // si no existe se muestra este texto
+    const clase = await this.claseRepository.findOne({
+      where: { id: claseId },
+    });
+    if (!clase) return 'error - no se encontró clase';
+    const clase_estudiante = await this.estudianteClaseRepository.findOne({
+      where: { claseId: claseId, estudianteId: estudianteId },
+    }); // si
+    if (clase_estudiante) return 'el estudiante ya tiene asignada esa clase';
+    return await this.estudianteClaseRepository.save(
+      new EstudianteClase(estudianteId, claseId),
+    );
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} estudiante`;
+  //** FIND ALL */
+  async findAll(): Promise<any> {
+    return await this.estudianteRepository.find();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} estudiante`;
+  
+  //**UPDATE  */
+
+  async update(EstudianteDto: EstudianteDto, id: number): Promise<string> {
+    const findEstudiante: FindOneOptions = { where: { id: id } };
+    let estudiante: Estudiante = await this.estudianteRepository.findOne(
+      findEstudiante,
+    );
+    if (!estudiante)
+      throw new Error('no se pudo encontrar el estudiante a modificar');
+    else {
+      let estudianteAnterior = estudiante.getNombre();
+      estudiante.setNombre(EstudianteDto.nombre);
+      estudiante = await this.estudianteRepository.save(estudiante); // save se encarga de distinguir entre create y update. Tiene las dos funciones y reconoce cuando debe crear y cuando modificar un recurso por el tipo de petición.
+      return `ok - ${estudianteAnterior} --> ${EstudianteDto.nombre}`;
+    }
+  }
+
+  //** REMOVE */
+  async remove(id: number) {
+    try {
+      const aux: FindOneOptions = { where: { id: id } };
+      let estudiante: Estudiante = await this.estudianteRepository.findOne(aux);
+      if (!estudiante) throw new Error('no se encontró el estudiante');
+      else {
+        await this.estudianteRepository.remove(estudiante);
+        return { id: id, message: 'se elimino exitosamente' }; // ->retorna json
+      }
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: ' ' + error,
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
   }
 }
